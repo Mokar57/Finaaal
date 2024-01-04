078ad6506e1268e25884a94e0a2e2783ba379b06
Mokar57 <emircansonmez@ogrenci.beykoz.edu.tr>
2024-01-04 17:00:50 +0300

    deneme

---
+++ Assets/Platformdaninme.cs
@@ -0,0 +1,53 @@
+
+using System.Collections;
+using UnityEngine;
+
+public class Platformdaninme : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D playerCollider;  //oyuncudan box collider çağırıyor
+    [SerializeField] public float beklemesuresi=0.5f;
+    
+
+    private GameObject tekyonplatform;   //tek yön platformları belirtiyor tag ile belirticez
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            if (tekyonplatform != null)
+            {
+                StartCoroutine(DisableCollision());
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "tekyonplatform")
+        {
+            tekyonplatform = other.gameObject;   //üzerine çıktığın platformu tek yön platform yapıyorum?
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "tekyonplatform")
+        {
+            tekyonplatform = null;  //üzerinde olmadığım platformu tek yön platformdan çıkarıyorum?
+        }
+    }
+
+    private IEnumerator DisableCollision()
+    {
+        BoxCollider2D platformCollider = tekyonplatform.GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+
+        yield return new WaitForSeconds(beklemesuresi);
+        
+        Physics2D.IgnoreCollision(playerCollider, platformCollider,false);
+        
+        //oyuncu ile platformun çarpışmasını yok say bekleme süresini bekle çarpışma yok saymayı kaldır
+    }
+    
+}
