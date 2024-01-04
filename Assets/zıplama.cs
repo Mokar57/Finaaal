507a28b561bd3080576283fc19fb0fa745c3839b
Mokar57 <emircansonmez@ogrenci.beykoz.edu.tr>
2024-01-04 22:25:22 +0300

    hareket check

--- Assets/zıplama.cs
+++ Assets/zıplama.cs
@@ -1,11 +1,14 @@
 
-
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Zıplama : MonoBehaviour
 {
 
   public float castdistance;
+
+  private bool yerdemi;
 
   public LayerMask groundLayer;
 
@@ -24,27 +27,52 @@ public class Zıplama : MonoBehaviour
 
   private void Update()
   {
-    if (Input.GetKeyDown(KeyCode.W) && Yerde())
+    
+    if (Input.GetKeyDown(KeyCode.W) && yerdemi)
     {
       rb.AddForce(new Vector2(0, jumpForce));
     }
   }
 
-  public bool Yerde()
-  {
-    if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castdistance, groundLayer))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-  }
+  // public bool Yerde()
+  // {
+  //   if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castdistance, groundLayer))
+  //   {
+  //     Debug.Log("yerde");
+  //     yerdemi = true;
+  //     return true;
+  //     
+  //   }
+  //   else
+  //   {
+  //     Debug.Log("yerde değil");
+  //     yerdemi = false;
+  //     return false;
+  //   }
+  // }
 
   private void OnDrawGizmos()
   {
     Gizmos.DrawWireCube(transform.position-transform.up * castdistance,boxSize);
+  }
+
+  private void OnCollisionStay2D(Collision2D other)
+  {
+    if (other.gameObject.CompareTag("tekyonplatform"))
+    {
+      Debug.Log("yerde");
+      yerdemi = true;
+    }
+    
+  }
+
+  private void OnCollisionExit2D(Collision2D other)
+  {
+    if (other.gameObject.CompareTag("tekyonplatform"))
+    {
+      Debug.Log("yerde değil");
+      yerdemi = false;
+    }
   }
 }
 
